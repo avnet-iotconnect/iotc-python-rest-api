1eacdc7485893020d2f2d08d737f5257dc0593ca
nik-markovic <nikola.m@gmail.com>
2025-03-22 18:03:00 -0500

    debug config issue

--- src/avnet/iotconnect/restapi/lib/config.py
+++ src/avnet/iotconnect/restapi/lib/config.py
@@ -114,7 +114,7 @@ def write() -> bool:
 
             global skey, pf, env, access_token, refresh_token, token_time, token_expiry
             section = get_section(SECTION_USER)
-            print(hex(username.encode('utf-8').hex()))
+            print(":".join("{:02x}".format(ord(c)) for c in username))
             section['username'] = username
             section['pf'] = pf
             section['env'] = env
